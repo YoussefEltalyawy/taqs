1ce935c92d62dd0565122843d60ac4dd52c8f522
Youssef Eltalyawy <yousefeltalyawy@gmail.com>
2024-03-07 20:36:27 +0200

    Data is now pulled from api and stored in object called weather data

---
+++ main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const apiKey = \"b40bbce6ac554f4fbf1142551240203\";\r\n// fetch(function(response) {\r\n// }).catch(function(err) {\r\n//   console.log(err);\r\n// })\r\nfetch(\r\n  \"https://api.weatherapi.com/v1/current.json?key=b40bbce6ac554f4fbf1142551240203&q=giza\"\r\n)\r\n  .then(function (respone) {})\r\n  .catch(function (err) {});\r\n\n\n//# sourceURL=webpack://taqs/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
